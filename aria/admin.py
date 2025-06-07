a7522598879eb5a4f1f3cf74f28f5bee3860319d
es3-dev <es3code@gmail.com>
2025-06-07 12:56:47 -0500

    Models añadidos en el admin.py

--- aria/admin.py
+++ aria/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Sede, Reunion, Asistencia
 # Register your models here.
+admin.site.register(Sede)
+admin.site.register(Reunion)
+admin.site.register(Asistencia)
