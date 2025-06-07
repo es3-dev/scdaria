d881c763692ffaab804256cd8b6f775f7c254314
es3-dev <es3code@gmail.com>
2025-06-07 12:47:24 -0500

    Primer commit | Creacion de los models

---
+++ aria/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Sede(models.Model):
+    nombre_sede = models.CharField(max_length=65)
+
+    def __str__(self):
+        return f'{self.nombre_sede}'
+    
+class Reunion(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    fecha = models.DateTimeField(auto_now_add=True)
+    sede = models.ForeignKey(Sede, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.titulo}'
+    
+class Asistencia(models.Model):
+    nombre = models.CharField(max_length=200)
+    documento = models.IntegerField()
+    correo = models.CharField(max_length=200)
+    vinculacion = models.CharField(max_length=200)
+    reunion = models.ForeignKey(Reunion, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.nombre} {self.documento} {self.correo}'
+
